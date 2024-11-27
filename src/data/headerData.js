4ccc10a7a9d04fd1abf1f4447fb6cee8760c909b
DEVESH VERMA <deveshverma@Mac.lan>
2024-11-28 01:48:33 +0530

    fix bug

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Devesh Verma',
     title: "Full-Stack Web Developer",
     desciption:"Passionate Full-Stack Web Developer adept in JavaScript, React, Node.js, and MongoDB, with a focus on crafting beautiful and functional websites. Proficient in HTML, CSS, and Bootstrap, I strive to deliver seamless user experiences through innovative design and industry-leading practices. Let's collaborate to bring your web projects to life with creativity and precision.",
-    image: 'https://i.ibb.co/TW2L73J/Whats-App-Image-2024-03-08-at-10-18-58-ea0a64b4-removebg-preview-1-removebg-preview.png',
+    image: 'https://i.ibb.co/PgXsmjC/final.jpg',
     resumePdf: resume
 }
